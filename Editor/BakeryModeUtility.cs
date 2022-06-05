a743bd7e6a3111123b5a0b760a2ee159a8e1dd74
z3y <33181641+z3y@users.noreply.github.com>
2022-06-05 23:11:47 +0200

    bakery mode switch utility

---
+++ Editor/BakeryModeUtility.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using UnityEditor;
+using UnityEngine;
+using System.Reflection;
+using NUnit.Framework;
+
+public class BakeryModeUtility : EditorWindow
+{
+    [MenuItem("Window/Bakery Mode Utility")]
+    private static void Init()
+    {
+        var window = (BakeryModeUtility)GetWindow(typeof(BakeryModeUtility));
+        window.Show();
+    }
+    
+    private static MethodInfo _getShaderGlobalKeywords = typeof(ShaderUtil).GetMethod("GetShaderGlobalKeywords", BindingFlags.Static | BindingFlags.NonPublic);
+    private static MethodInfo _getShaderLocalKeywords = typeof(ShaderUtil).GetMethod("GetShaderLocalKeywords", BindingFlags.Static | BindingFlags.NonPublic);
+    private static readonly int BakeryLightmapMode = Shader.PropertyToID("bakeryLightmapMode");
+
+
+    private static Dictionary<Material, int> FindSupportedObjects(bool checkPropertyBlocks = false)
+    {
+        var renderers = FindObjectsOfType<Renderer>().ToList();
+        
+        
+        var materials = renderers.SelectMany(x => x.sharedMaterials).Distinct().ToList();
+        var supportedMaterials = new List<Material>();
+
+        foreach (var material in materials)
+        {
+            if (material.shader is null) continue;
+
+            var globalKeywords = (string[])_getShaderGlobalKeywords.Invoke(null, new object[] { material.shader });
+            var localKeywords = (string[])_getShaderLocalKeywords.Invoke(null, new object[] { material.shader });
+
+            
+            if (!globalKeywords.Contains("BAKERY_SH") && !globalKeywords.Contains("BAKERY_RNM") &&
+                !localKeywords.Contains("BAKERY_SH") && !localKeywords.Contains("BAKERY_RNM"))
+            {
+                continue;
+            }
+            
+            supportedMaterials.Add(material);
+        }
+
+        if (!checkPropertyBlocks) return supportedMaterials.ToDictionary(m => m, m => 0);
+
+        
+        var storage = ftRenderLightmap.FindRenderSettingsStorage();
+        var bakeryMaterials = new Dictionary<Material, int>();
+        
+        foreach (var renderer in renderers)
+        {
+            if (!renderer.HasPropertyBlock()) continue;
+            
+            var block = new MaterialPropertyBlock();
+            renderer.GetPropertyBlock(block);
+            int bakeryMode = storage.renderSettingsRenderDirMode != 0 ? block.GetInt(BakeryLightmapMode) : 0;
+            
+            foreach (var rendererSharedMaterial in renderer.sharedMaterials)
+            {
+                if (!supportedMaterials.Contains(rendererSharedMaterial)) continue;
+                
+                Debug.Log("Adding");
+                
+                bakeryMaterials.Add(rendererSharedMaterial, bakeryMode);
+            }
+        }
+        
+        Debug.Log(bakeryMaterials.Count());
+
+
+        return bakeryMaterials;
+    }
+
+    private static void ToggleKeyword(Material material, string keyword, bool enable)
+    {
+        if (enable) material.EnableKeyword(keyword);
+        else material.DisableKeyword(keyword);
+    }
+
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("From Property Blocks"))
+        {
+            var bakeryMaterials = FindSupportedObjects(true);
+
+            foreach (var material in bakeryMaterials.Keys)
+            {
+                ToggleKeyword(material, "BAKERY_SH", bakeryMaterials[material] == 3);
+                ToggleKeyword(material, "BAKERY_RNM", bakeryMaterials[material] == 2);
+
+                if (bakeryMaterials[material] == 0)
+                {
+                    material.DisableKeyword("BAKERY_SH");
+                    material.DisableKeyword("BAKERY_RNM");
+                }
+
+            }
+        }
+        
+        if (GUILayout.Button("SH"))
+        {
+            var bakeryMaterials = FindSupportedObjects();
+            foreach (var material in bakeryMaterials.Keys)
+            {
+                material.EnableKeyword("BAKERY_SH");
+                material.DisableKeyword("BAKERY_RNM");
+            }
+        }
+        
+        if (GUILayout.Button("RNM"))
+        {
+            var bakeryMaterials = FindSupportedObjects();
+            foreach (var material in bakeryMaterials.Keys)
+            {
+                material.EnableKeyword("BAKERY_RNM");
+                material.DisableKeyword("BAKERY_SH");
+            }
+        }
+        
+        if (GUILayout.Button("Disable"))
+        {
+            var renderers = FindObjectsOfType<Renderer>().ToList();
+            var materials = renderers.SelectMany(x => x.sharedMaterials).Distinct().ToList();
+            
+            foreach (var material in materials)
+            {
+                material.DisableKeyword("BAKERY_SH");
+                material.DisableKeyword("BAKERY_RNM");
+            }
+        }
+        
+        
+    }
+}
