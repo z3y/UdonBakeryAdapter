0936a1b36f44e5995c858764c333e831bacffcfa
z3y <33181641+z3y@users.noreply.github.com>
2022-08-04 17:21:32 +0200

    Update BakeryModeUtility.cs

--- Editor/BakeryModeUtility.cs
+++ Editor/BakeryModeUtility.cs
@@ -6,7 +6,6 @@ using UnityEditor;
 using UnityEngine;
 using System.Reflection;
 
-[InitializeOnLoad]
 public class BakeryModeUtility : EditorWindow
 {
     [MenuItem("Window/Bakery Mode Utility")]
@@ -16,30 +15,24 @@ public class BakeryModeUtility : EditorWindow
         window.Show();
     }
     
-    static BakeryModeUtility() => ftRenderLightmap.OnFinishedFullRender += AutoSwitch;
-    
     private static MethodInfo _getShaderGlobalKeywords = typeof(ShaderUtil).GetMethod("GetShaderGlobalKeywords", BindingFlags.Static | BindingFlags.NonPublic);
     private static MethodInfo _getShaderLocalKeywords = typeof(ShaderUtil).GetMethod("GetShaderLocalKeywords", BindingFlags.Static | BindingFlags.NonPublic);
     
-    private static readonly int BakeryLightmapMode = Shader.PropertyToID("bakeryLightmapMode");
-
-    private const string AutoSwitchPref = "BakeryModeUtilityAutoSwitch";
-
-    private static bool _autoSwitch = false;
-
-    private bool _firstTime = true;
     private void OnGUI()
     {
-        if (_firstTime)
+
+        if (GUILayout.Button(new GUIContent("Mono SH", "Enable BAKERY_MONOSH on all materials if the shader supports it")))
         {
-            _autoSwitch = EditorPrefs.GetBool(AutoSwitchPref, false);
-            _firstTime = false;
+            var bakeryMaterials = FindSupportedObjects();
+            foreach (var material in bakeryMaterials.Keys)
+            {
+                material.DisableKeyword("BAKERY_SH");
+                material.DisableKeyword("BAKERY_RNM");
+                material.EnableKeyword("BAKERY_MONOSH");
+                ToggleProperties(material, false, false, true);
+            }
         }
-        if (GUILayout.Button(new GUIContent("From Property Blocks", "Toggle BAKERY_SH or BAKERY_RNM depending on bakeryLightmapMode from property blocks on all materials if the shader supports it")))
-        {
-            SetFromPropertyBlocks();
-        }
-        
+
         if (GUILayout.Button(new GUIContent("SH", "Enable BAKERY_SH on all materials if the shader supports it")))
         {
             var bakeryMaterials = FindSupportedObjects();
@@ -47,7 +40,8 @@ public class BakeryModeUtility : EditorWindow
             {
                 material.EnableKeyword("BAKERY_SH");
                 material.DisableKeyword("BAKERY_RNM");
-                ToggleProperties(material, true, false);
+                material.DisableKeyword("BAKERY_MONOSH");
+                ToggleProperties(material, true, false, false);
             }
         }
         
@@ -58,23 +52,17 @@ public class BakeryModeUtility : EditorWindow
             {
                 material.EnableKeyword("BAKERY_RNM");
                 material.DisableKeyword("BAKERY_SH");
-                ToggleProperties(material, false, true);
+                material.DisableKeyword("BAKERY_MONOSH");
+                ToggleProperties(material, false, true, false);
             }
         }
         
-        if (GUILayout.Button(new GUIContent("Disable", "Disable BAKERY_SH and BAKERY_RNM on all materials")))
+        if (GUILayout.Button(new GUIContent("Disable", "Disable BAKERY_SH, BAKERY_RNM and BAKERY_MONOSH on all materials")))
         {
             DisableAll();
         }
-        
-        EditorGUI.BeginChangeCheck();
-        _autoSwitch = EditorGUILayout.ToggleLeft(new GUIContent("Auto Switch", "Toggle BAKERY_SH or BAKERY_RNM automatically after the bake is finished based on property blocks"), _autoSwitch);
-        if (EditorGUI.EndChangeCheck())
-        {
-            EditorPrefs.SetBool(AutoSwitchPref, _autoSwitch);
-        }
 
-        EditorGUILayout.HelpBox(new GUIContent("Supports shaders with BAKERY_SH or BAKERY_RNM keyword. Toggles on some materials will not be updated visually, only keywords get applied. Keywords on materials are visible in debug mode"));
+        EditorGUILayout.HelpBox(new GUIContent("Only supports shaders with BAKERY_SH, BAKERY_RNM or BAKERY_MONOSH keyword. Toggles on some materials will not be updated visually, only keywords get applied"));
     }
 
     private static void DisableAll()
@@ -87,11 +75,12 @@ public class BakeryModeUtility : EditorWindow
             if (material is null) continue;
             material.DisableKeyword("BAKERY_SH");
             material.DisableKeyword("BAKERY_RNM");
-            ToggleProperties(material, false, false);
+            material.DisableKeyword("BAKERY_MONOSH");
+            ToggleProperties(material, false, false, false);
         }
     }
 
-    private static Dictionary<Material, int> FindSupportedObjects(bool checkPropertyBlocks = false)
+    private static Dictionary<Material, int> FindSupportedObjects()
     {
         var renderers = FindObjectsOfType<Renderer>().ToList();
         
@@ -116,39 +105,18 @@ public class BakeryModeUtility : EditorWindow
             supportedMaterials.Add(material);
         }
 
-        if (!checkPropertyBlocks) return supportedMaterials.ToDictionary(m => m, m => 0);
-
-        
-        var storage = ftRenderLightmap.FindRenderSettingsStorage();
-        var bakeryMaterials = new Dictionary<Material, int>();
-
-        foreach (var renderer in renderers)
-        {
-            if (!renderer.HasPropertyBlock()) continue;
-            
-            var block = new MaterialPropertyBlock();
-            renderer.GetPropertyBlock(block);
-            int bakeryMode = storage.renderSettingsRenderDirMode > 2 ? block.GetInt(BakeryLightmapMode) : 0;
-            
-            foreach (var rendererSharedMaterial in renderer.sharedMaterials)
-            {
-                if (!supportedMaterials.Contains(rendererSharedMaterial) || bakeryMaterials.ContainsKey(rendererSharedMaterial)) continue;
-
-                bakeryMaterials.Add(rendererSharedMaterial, bakeryMode);
-            }
-        }
-
-        return bakeryMaterials;
+        return supportedMaterials.ToDictionary(m => m, m => 0);
     }
 
 
     // tries to set the float on common property names
-    private static void ToggleProperties(Material material, bool SH, bool RNM)
+    private static void ToggleProperties(Material material, bool SH, bool RNM, bool MonoSH)
     {
         material.SetFloat("_BAKERY_SH", SH ? 1 : 0);
         material.SetFloat("_BAKERY_RNM", RNM ? 1 : 0);
+        material.SetFloat("_BAKERY_MONOSH", MonoSH ? 1 : 0);
 
-        if (!SH && !RNM)
+        if (!SH && !RNM && !MonoSH)
         {
             material.SetFloat("Bakery", 0);
             material.SetFloat("_BakeryMode", 0);
@@ -166,41 +134,13 @@ public class BakeryModeUtility : EditorWindow
             material.SetFloat("_BakeryMode", 2);
             material.SetFloat("_Bakery", 2);
         }
-
-    }
-
-    private static void ToggleKeyword(Material material, string keyword, bool enable)
-    {
-        if (enable) material.EnableKeyword(keyword);
-        else material.DisableKeyword(keyword);
-    }
-
-    private static void SetFromPropertyBlocks()
-    {
-        DisableAll();
-        var bakeryMaterials = FindSupportedObjects(true);
-
-        foreach (var material in bakeryMaterials.Keys)
+        else if (MonoSH)
         {
-            ToggleKeyword(material, "BAKERY_SH", bakeryMaterials[material] == 3);
-            ToggleKeyword(material, "BAKERY_RNM", bakeryMaterials[material] == 2);
-
-            ToggleProperties(material, bakeryMaterials[material] == 3, bakeryMaterials[material] == 2);
-
-            if (bakeryMaterials[material] == 0)
-            {
-                material.DisableKeyword("BAKERY_SH");
-                material.DisableKeyword("BAKERY_RNM");
-            }
+            material.SetFloat("Bakery", 3);
+            material.SetFloat("_BakeryMode", 3);
+            material.SetFloat("_Bakery", 3);
         }
-    }
 
-    private static void AutoSwitch(object sender, EventArgs e)
-    {
-        if (!EditorPrefs.GetBool(AutoSwitchPref, false)) return;
-        
-        SetFromPropertyBlocks();
-        Debug.Log("<color=fuchsia>[BakeryModeUtility]</color> Bakery Keywords Applied"); 
     }
 }
 #endif
